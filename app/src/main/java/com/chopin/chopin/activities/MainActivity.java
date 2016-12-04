ddb1ba15847b06242e3598f2f29e419694401eee
bla bla <bartek.blazik@gmail.com>
2016-12-05 00:42:57 +0100

    add fragment for map

--- app/src/main/java/com/chopin/chopin/activities/MainActivity.java
+++ app/src/main/java/com/chopin/chopin/activities/MainActivity.java
@@ -1,77 +1,47 @@
 package com.chopin.chopin.activities;
 
-import android.Manifest;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.chopin.chopin.API.API;
 import com.chopin.chopin.R;
 import com.chopin.chopin.fragments.AddOffer;
+import com.chopin.chopin.fragments.FragmentWithMap;
 import com.chopin.chopin.fragments.MyChippedList;
 import com.chopin.chopin.fragments.MyOfferList;
 import com.chopin.chopin.fragments.OfferList;
 import com.chopin.chopin.models.Offer;
 import com.chopin.chopin.models.User;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener,
-    OnMapReadyCallback,
-    GoogleApiClient.ConnectionCallbacks,
-    GoogleApiClient.OnConnectionFailedListener,
-    GoogleMap.OnMyLocationButtonClickListener
+    implements NavigationView.OnNavigationItemSelectedListener
     {
 
-        private android.support.v4.app.FragmentManager fragmentManager;
-    private API.APIInterface apiInterface;
+    private android.support.v4.app.FragmentManager fragmentManager;
+    public static API.APIInterface apiInterface;
     private User user;
-    private ArrayList<MarkerOptions> markers;
-    private API.APIInterface _api;
     private ArrayList<Offer> offers;
     private MapFragment mMapFragment;
-    private GoogleApiClient mGoogleApiClient = null;
-        private GoogleMap mGoogleMap;
-    final private int MY_REQUEST_FINE_LOCATION = 124;
-    final private int MY_REQUEST_COARSE_LOCATION = 125;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,18 +50,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         apiInterface = API.getClient();
-        _api = API.getClient();
         mMapFragment = null;
-
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-        mGoogleApiClient.connect();
         authorization();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -149,7 +108,6 @@ public class MainActivity extends AppCompatActivity
         // The activity is about to become visible.
         Fragment fragment;
         fragment = new OfferList();
-        mGoogleApiClient.connect();
 
         fragmentManager = getSupportFragmentManager();
         fragmentManager
@@ -170,11 +128,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public boolean isGooglePlayServicesAvailable(Context context) {
-        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(context);
-        return resultCode == ConnectionResult.SUCCESS;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -220,16 +173,7 @@ public class MainActivity extends AppCompatActivity
                 fragment = new MyChippedList();
                 break;
             case R.id.nav_MyMap:
-                mMapFragment = MapFragment.newInstance();
-                mMapFragment.getMapAsync(this);
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_content, mMapFragment)
-                        .addToBackStack(mMapFragment.toString())
-                        .commit();
-
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
-                break; //TODO fix
+               fragment = new FragmentWithMap();
         }
 
         fragmentManager = getSupportFragmentManager();
@@ -242,141 +186,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    public void onMapReady(final GoogleMap map) {
-
-        try {
-            mGoogleMap = map;
-            mGoogleMap.setMyLocationEnabled(true);
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                List<String> permissionsNeeded = new ArrayList<>();
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_REQUEST_COARSE_LOCATION);
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_REQUEST_FINE_LOCATION);
-            }
-
-            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            if (mLastLocation != null && mGoogleMap != null) {
-                Toast.makeText(this, "MyLocation button clicked," + mLastLocation.getLatitude()+","+ mLastLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))      // Sets the center of the map to location user
-                        .zoom(17)                   // Sets the zoom
-                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
-                        .build();                   // Creates a CameraPosition from the builder
-                mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        } catch (SecurityException s) {
-            Log.e("Chopin$MainActivity", s.getMessage());
-        }
-
-        Call<List<Offer>> query = _api.getAllOffers();
-        offers = new ArrayList<>();
-        markers = new ArrayList<>();
-        query.enqueue(new Callback<List<Offer>>() {
-            @Override
-            public void onResponse(Call<List<Offer>> call, Response<List<Offer>> response) {
-                if (response.isSuccessful()) {
-                    offers.addAll(response.body());
-                    for (int i = 0; i < offers.size(); i++) {
-                        markers.add(new MarkerOptions()
-                                .position(new LatLng(offers.get(i).getLatitude(), offers.get(i).getLongitude()))
-                                .title(offers.get(i).getDescription()));
-                        for (int j = 0; j < markers.size(); j++) {
-                            mGoogleMap.addMarker(markers.get(i));
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Offer>> call, Throwable t) {
-            }
-        });
-        for (int i = 0; i < markers.size(); i++) {
-            mGoogleMap.addMarker(markers.get(i));
-        }
-
-        /*
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
-                */
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-
-        return false;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private boolean addPermission(List<String> permissionsList, String permission) {
-        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
-            permissionsList.add(permission);
-            // Check for Rationale Option
-            if (!shouldShowRequestPermissionRationale(permission))
-                return false;
-        }
-        return true;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_REQUEST_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-
-            }
-            case MY_REQUEST_COARSE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/chopin/chopin/fragments/FragmentWithMap.java
@@ -0,0 +1,253 @@
+package com.chopin.chopin.fragments;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.chopin.chopin.R;
+import com.chopin.chopin.activities.MainActivity;
+import com.chopin.chopin.models.Offer;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class FragmentWithMap extends Fragment implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMyLocationButtonClickListener,
+        LocationListener {
+
+    private ArrayList<MarkerOptions> markers;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient mGoogleApiClient = null;
+    final private int MY_REQUEST_FINE_LOCATION = 124;
+    final private int MY_REQUEST_COARSE_LOCATION = 125;
+    private ArrayList<Offer> offers;
+    private static SupportMapFragment mapFragment;
+    public Location mLastLocation;
+    public LocationRequest mLocationRequest;
+    public FragmentWithMap() {
+        // Required empty public
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static FragmentWithMap newInstance() {
+        FragmentWithMap fragment = new FragmentWithMap();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mapFragment = SupportMapFragment.newInstance();
+        mapFragment.getMapAsync(this);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_content, mapFragment)
+                .commit();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mapFragment = new SupportMapFragment();
+        // Create an instance of GoogleAPIClient.1
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        try {
+            mGoogleMap = map;
+            mGoogleMap.setMyLocationEnabled(true);
+
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_REQUEST_COARSE_LOCATION);
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_REQUEST_FINE_LOCATION);
+            }
+
+        } catch (SecurityException s) {
+            Log.e("Chopin$MainActivity", s.getMessage());
+        }
+
+        Call<List<Offer>> query = MainActivity.apiInterface.getAllOffers();
+        offers = new ArrayList<>();
+        markers = new ArrayList<>();
+        query.enqueue(new Callback<List<Offer>>() {
+            @Override
+            public void onResponse(Call<List<Offer>> call, Response<List<Offer>> response) {
+                if (response.isSuccessful()) {
+                    offers.addAll(response.body());
+                    for (int i = 0; i < offers.size(); i++) {
+                        markers.add(new MarkerOptions()
+                                .position(new LatLng(offers.get(i).getLatitude(), offers.get(i).getLongitude()))
+                                .title(offers.get(i).getDescription()));
+                        for (int j = 0; j < markers.size(); j++) {
+                            mGoogleMap.addMarker(markers.get(i));
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Offer>> call, Throwable t) {
+            }
+        });
+        for (int i = 0; i < markers.size(); i++) {
+            mGoogleMap.addMarker(markers.get(i));
+        }
+
+        /*
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+                */
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+
+        return false;
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+    public boolean isGooglePlayServicesAvailable(Context context) {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(context);
+        return resultCode == ConnectionResult.SUCCESS;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+
+            }
+            case MY_REQUEST_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        if (mLastLocation != null && mGoogleMap != null) {
+            Toast.makeText(getActivity(), "MyLocation button clicked," + mLastLocation.getLatitude()+","+ mLastLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+}
